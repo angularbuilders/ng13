1072eb2519ef062f62ef8744ac2d8d9a73d545e2
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-24 17:14:41 +0200

    chore: rename ng 13

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'may22'`, () => {
+  it(`should have as title 'ng13'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('may22');
+    expect(app.title).toEqual('ng13');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('may22 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'ng13 app is running!'
+    );
   });
 });
