e0f31bd8626d1a8de2c6abad0bb55009fac3f45f
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-26 18:19:51 +0200

    feat: trip details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -102,6 +102,15 @@ export class HomeComponent implements OnInit {
     }
     return 'orange';
   }
+  public getClassForPlaces(places: number) {
+    if (places === 0) {
+      return 'sold-out';
+    }
+    if (places < 8) {
+      return 'few-places';
+    }
+    return '';
+  }
 
   public ngOnInit(): void {}
 }
