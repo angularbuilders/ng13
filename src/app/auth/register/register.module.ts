1fb2db6bd15f91f69f31de419b4050fcea7105c4
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-31 14:11:56 +0200

    fix: declarations of page form components

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { RegisterRoutingModule } from './register-routing.module';
+import { RegisterForm } from './register.form';
 import { RegisterPage } from './register.page';
 
 @NgModule({
-  declarations: [RegisterPage],
-  imports: [CommonModule, RegisterRoutingModule],
+  declarations: [RegisterPage, RegisterForm],
+  imports: [CommonModule, RegisterRoutingModule, SharedModule],
 })
 export class RegisterModule {}
