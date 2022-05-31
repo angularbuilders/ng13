008395598afef972fd052786f6e72128ec332cc4
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-31 17:57:25 +0200

    feat:  new agency form

--- src/app/agencies/new-agency/new-agency.module.ts
+++ src/app/agencies/new-agency/new-agency.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { NewAgencyRoutingModule } from './new-agency-routing.module';
-import { NewAgencyPage } from './new-agency.page';
 import { NewAgencyForm } from './new-agency.form';
+import { NewAgencyPage } from './new-agency.page';
 
 @NgModule({
   declarations: [NewAgencyPage, NewAgencyForm],
-  imports: [CommonModule, NewAgencyRoutingModule],
+  imports: [CommonModule, NewAgencyRoutingModule, SharedModule],
 })
 export class NewAgencyModule {}
