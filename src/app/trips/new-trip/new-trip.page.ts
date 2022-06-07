34f3412fdfb6e308dd88343a46c36419a2f1698b
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-07 18:54:29 +0200

    feat: new trip form

---
+++ src/app/trips/new-trip/new-trip.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './new-trip.page.html',
+  styleUrls: ['./new-trip.page.css'],
+})
+export class NewTripPage implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
