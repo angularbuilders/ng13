2841181f06602fff9db79d5ebc0fbf20f6b5957b
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-08 17:02:46 +0200

    feat: trip service $

--- src/app/core/api/trips.api.ts
+++ src/app/core/api/trips.api.ts
@@ -1,82 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Trip } from './trip.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TripsApi {
-  private trips: Trip[] = [
-    {
-      id: 'space-y-moon-1',
-      agencyId: 'space-y',
-      agencyTripCode: 'moon',
-      destination: 'The Moon',
-      places: 14,
-      startDate: '2023-01-01',
-      endDate: '2023-02-01',
-      flightPrice: 1200000,
-      stayingNightPrice: 10000,
-      kind: 'WithStay',
-      status: 'Confirmed',
-      extraLuggagePricePerKilo: 2000,
-      premiumFoodPrice: 0,
-    },
-    {
-      id: 'space-y-mars-2',
-      agencyId: 'space-y',
-      agencyTripCode: 'mars',
-      destination: 'Mars',
-      places: 8,
-      startDate: '2024-01-01',
-      endDate: '2024-05-01',
-      flightPrice: 8400000,
-      stayingNightPrice: 10000,
-      kind: 'WithStay',
-      status: 'Waiting',
-      extraLuggagePricePerKilo: 50000,
-      premiumFoodPrice: 0,
-    },
-    {
-      id: 'green-origin-low-orbit-3',
-      agencyId: 'green-origin',
-      agencyTripCode: 'low-orbit',
-      destination: 'Low Orbit',
-      places: 0,
-      startDate: '2022-04-01',
-      endDate: '2022-04-01',
-      flightPrice: 320000,
-      stayingNightPrice: 0,
-      kind: 'TripOnly',
-      status: 'Waiting',
-      extraLuggagePricePerKilo: 0,
-      premiumFoodPrice: 1000,
-    },
-    {
-      id: 'green-origin-iss-4',
-      agencyId: 'green-origin',
-      agencyTripCode: 'iss',
-      destination: 'ISS',
-      places: 6,
-      startDate: '2022-06-01',
-      endDate: '2022-06-01',
-      flightPrice: 800000,
-      stayingNightPrice: 0,
-      kind: 'TripOnly',
-      status: 'Waiting',
-      extraLuggagePricePerKilo: 0,
-      premiumFoodPrice: 1000,
-    },
-  ];
+  private apiUrl = 'http://localhost:3000';
 
-  public getAll() {
-    return this.trips;
+  constructor(private http: HttpClient) {}
+
+  public getAll$(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.apiUrl + '/agencies');
+  }
+  public getById$(id: string): Observable<Trip> {
+    return this.http.get<Trip>(this.apiUrl + '/agencies/' + id);
   }
 
-  public getById(id: string) {
-    return this.trips.find((t) => t.id === id);
-  }
-
-  public post(trip: Partial<Trip>) {
-    this.trips.push(trip as Trip);
+  public post$(trip: Trip): Observable<Trip> {
+    return this.http.post<Trip>(this.apiUrl + '/agencies', trip);
   }
 }
